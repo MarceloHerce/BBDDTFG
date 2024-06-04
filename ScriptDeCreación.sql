02b855cda95973d1509b6c844cba373ca024605a
MarceloHerce <marcelohercesanz@gmail.com>
2024-06-04 08:42:33 +0200

    modificaciones

--- ScriptDeCreación.sql
+++ ScriptDeCreación.sql
@@ -12,10 +12,16 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL,
     user_password VARCHAR(255) NOT NULL,
     salt VARCHAR(255),
-    access_type_id BIGINT NOT NULL,
+    x_access_type_id BIGINT NOT NULL,
     -- f2a_enabled VARCHAR(255),
     -- f2a_secret VARCHAR(255)
-    CONSTRAINT fk_access_type FOREIGN KEY (access_type_id) REFERENCES access_type(access_type_id)
+    CONSTRAINT fk_access_type FOREIGN KEY (x_access_type_id) REFERENCES access_type(access_type_id)
+)AUTO_INCREMENT = 1;
+CREATE TABLE IF NOT EXISTS users_media (
+	media_id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    x_user_id BIGINT NOT NULL,
+    media_reference VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY (x_user_id) REFERENCES users(user_id)
 );
 
 
